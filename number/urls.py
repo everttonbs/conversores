fd6c3b5674f85f0346d3c0c50725a3a7720342d3
everttonbs <ebs@ic.ufal.br>
2021-07-11 11:25:44 -0300

    Implementado a conversao decimal para numero bin e hex

--- number/urls.py
+++ number/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.conversor_num),
-    path('binario', views.binario),
-    path('hexadecimal', views.hexadecimal),
+    
 ]
--- number/views.py
+++ number/views.py
@@ -5,15 +5,65 @@ from django.shortcuts import render
 def conversor_num(request):
     if 'number'in request.GET:
         decimal_number = request.GET['number']
+
+        if decimal_number != '':
+            if 'to_convert' in request.GET:
+                if request.GET['to_convert'] == 'binario':
+                    convert_number = _get_binario(int(decimal_number))
+                elif request.GET['to_convert'] == 'hexadecimal':
+                    convert_number = _get_hexadecimal(int(decimal_number))
+                else:
+                    context = None
+
+                context = {
+                    'decimal_number' : decimal_number,
+                    'convert_number' : convert_number,
+                    'type'           : request.GET['to_convert'],
+                }
+
+            else:
+                context = None
+        else:
+            context = None
     else:
         context = None
 
-    return render(request, 'number/conversor_num.html')
+    return render(request, 'number/conversor_num.html', context)
 
-def binario(request):
+def _get_binario(dec_num):
+    num_bin = list()
+    while dec_num > 1:            
+        num_bin.insert(0, dec_num % 2)
+        dec_num = dec_num // 2
+    num_bin.insert(0, dec_num)
+        
+    return num_bin
 
-    return render(request, 'number/binario.html')
+def _get_hexadecimal(dec_num):
+    hex_number = list()
+    dict_hex = {
+        0: 0,
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 'A',
+        11: 'B',
+        12: 'C',
+        13: 'D',
+        14: 'E',
+        15: 'F'
+    }
 
-def hexadecimal(request):
+    while dec_num > 15:
+        hex_number.insert(0, dict_hex[dec_num%16])
+        dec_num = dec_num // 16
+    hex_number.insert(0, dict_hex[dec_num])
+    
+    return hex_number
 
-    return render(request, 'number/hexadecimal.html')
