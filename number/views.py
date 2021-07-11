56a97cd6c5c20517b113d795356cbde43973146f
everttonbs <ebs@ic.ufal.br>
2021-07-11 00:45:46 -0300

    Implementação da escolha de saída do usuário

--- number/views.py
+++ number/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def conversor_num(request):
+    if 'number'in request.GET:
+        decimal_number = request.GET['number']
+    else:
+        context = None
+
     return render(request, 'number/conversor_num.html')
 
 def binario(request):
